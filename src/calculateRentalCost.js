178d4af55359b19dd4fcfa8af6e4837e85a3ebb0
Your Name <you@example.com>
2025-07-31 00:08:56 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const dailyRate = 40;
+  const totalCost = days * dailyRate;
+  const shortTerm = 3;
+  const shortTermDiscount = 20;
+  const longerm = 7;
+  const longTermDiscount = 50;
+
+  if (days < shortTerm) {
+    return totalCost;
+  }
+
+  if (days < longerm) {
+    return totalCost - shortTermDiscount;
+  }
+
+  return totalCost - longTermDiscount;
 }
 
 module.exports = calculateRentalCost;
